d6c3d48367a18dc19b4ee4adbe7e31b245347956
Shanmugapriya s <sshanmugapriya0096@gmail.com>
2025-05-25 12:13:33 +0530

    Sathya code is done

---
+++ src/Polymorphism/MethodA.java
@@ -0,0 +1,30 @@
+package Polymorphism;
+
+public class MethodA {
+
+	public void overLoading(int e) {
+		System.out.println(e);
+	}
+	public void overLoading(String a) {
+		System.out.println(a);
+	}
+	public void overLoading (String b, int f) {
+		System.out.println(f);
+	}
+	public void overLoading (int a, int b) {
+		int sum= a+b;
+		System.out.println(sum);
+	}
+    
+	public static void main(String[] args) {
+		
+		MethodOverloading obj=new MethodOverloading ();
+		
+		obj.overLoading(50);
+		obj.overLoading("priya");
+		obj.overLoading(50,60);
+		obj.overLoading("Test", 10);
+	}
+		
+}
+
